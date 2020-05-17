9ff57ed749a9f84e0f232ff6d85ee4d2ff5a22d2
GillianY1 <gilliangilliany@gmail.com>
2020-05-17 16:43:59 +0800

    add hamberger menu on FAQ.html

--- js/all.js
+++ js/all.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    
 
+    /*---header ham menu-----*/
+    $('.ham-icon').click(function(e){
+        e.preventDefault();
+        $('.header-menu').toggleClass('active');
+    });
 });
